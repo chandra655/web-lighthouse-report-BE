143f4deeebe230043c1d82cfbb2cc73b80615f0c
chandra655 <chandra.penugonda@unacademy.com>
2020-10-26 19:47:18 +0530

    use node-schedule instaed of node-cron

--- src/server.js
+++ src/server.js
@@ -3,7 +3,8 @@ import path from "path";
 require("dotenv").config({ path: path.join(__dirname, "../.env.local") });
 
 import express from "express";
-import cron from "node-cron";
+import schedule from "node-schedule";
+// import cron from "node-cron";
 import cors from "cors";
 import crypto from "crypto";
 import nodemailer from "nodemailer";
@@ -21,7 +22,7 @@ app.use(cors());
 
 const piDB = firebase.firestore().collection("performance-results");
 
-cron.schedule("0 0 */6 * * *", () => {
+schedule.scheduleJob("0 */1 * * * *", function () {
   const messageOptions = {
     from: "chandrapenugonda655@gmail.com",
     to: "chandra.penugonda@unacademy.com",
@@ -46,22 +47,49 @@ cron.schedule("0 0 */6 * * *", () => {
       console.log("Email successfully sent!");
     }
   });
-  urls.forEach((url) => {
-    setTimeout(() => {
-      getInsight(url).then(async (data) => {
-        if (data) {
-          const output = await piDB.add({
-            ...data,
-            date: getDate(),
-            routeId: crypto.createHash("sha1").update(url).digest("hex"),
-          });
-        } else {
-          console.log("error");
-        }
-      });
-    }, 60 * 1000);
-  });
 });
+
+// cron.schedule("0 */6 * * *", () => {
+//   const messageOptions = {
+//     from: "chandrapenugonda655@gmail.com",
+//     to: "chandra.penugonda@unacademy.com",
+//     subject: "Scheduled Email",
+//     text: `Hi there. This email was automatically sent by us. ${new Date()}`,
+//   };
+
+//   const transporter = nodemailer.createTransport({
+//     service: "gmail",
+//     host: "smtp.gmail.com",
+//     secure: false,
+//     auth: {
+//       user: process.env.email,
+//       pass: process.env.password,
+//     },
+//   });
+
+//   transporter.sendMail(messageOptions, function (error, info) {
+//     if (error) {
+//       throw error;
+//     } else {
+//       console.log("Email successfully sent!");
+//     }
+//   });
+//   urls.forEach((url) => {
+//     setTimeout(() => {
+//       getInsight(url).then(async (data) => {
+//         if (data) {
+//           const output = await piDB.add({
+//             ...data,
+//             date: getDate(),
+//             routeId: crypto.createHash("sha1").update(url).digest("hex"),
+//           });
+//         } else {
+//           console.log("error");
+//         }
+//       });
+//     }, 60 * 1000);
+//   });
+// });
 
 app.use("/api", apiRouter);
 
